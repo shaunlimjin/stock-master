47b4550b643a3ae0acacaf244f7dd07a8ad7226c
Shaun Lim <shaunthegreat@gmail.com>
2012-09-04 00:20:42 -0700

    -Removed use of Morphia ORM. Using native drivers instead. -- MongoRecorder now uses native drivers and stores date.

--- stockmaster/StockMaster.java
+++ stockmaster/StockMaster.java
@@ -66,17 +66,13 @@ public class StockMaster {
         //StockMaster stockMaster = new StockMaster(new ReplayCSVMarketDataImpl("FeedData/","20120903","SGX"));
 
         // Starts the application using Mongo Replayer
-        StockMaster stockMaster = new StockMaster(new ReplayMongoMarketDataImpl("sgx", Log.getFormattedDateTime("20120903 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
+        //StockMaster stockMaster = new StockMaster(new ReplayMongoMarketDataImpl("sgx", Log.getFormattedDateTime("20120903 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
 
 		//Define recorder to use with marketData
-		//Datetime format yyyyMMdd hh:mm:ss 
-<<<<<<< HEAD
-		stockMaster.loadRecorder(new CSVFileRecorderImpl("", "SGX", Log.getFormattedDateTime("20120904 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
-		//stockMaster.loadRecorder(new MongoRecorderImpl("sgx", Log.getFormattedDateTime("20120904 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
-=======
+		//Datetime format yyyyMMdd hh:mm:ss
+		//stockMaster.loadRecorder(new CSVFileRecorderImpl("", "SGX", Log.getFormattedDateTime("20120904 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
 		//stockMaster.loadRecorder(new CSVFileRecorderImpl("", "Random", Log.getFormattedDateTime("20120904 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
-		//stockMaster.loadRecorder(new MongoRecorderImpl("sgx", Log.getFormattedDateTime("20120903 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
->>>>>>> - Refactored MongoDB methods into Singleton class, MongoManager.
+		stockMaster.loadRecorder(new MongoRecorderImpl("sgx", Log.getFormattedDateTime("20120904 08:58:00"), Log.getFormattedDateTime("20120904 17:05:00")));
 		
 		// Define algorithm stock manager would use to monitor the market
         stockMaster.loadAlgo(new RideTheTideImpl(stockMaster));	
